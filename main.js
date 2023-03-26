f1e13180adc45988dad0cfe51fd7e137a81020e6
gabrielMFCR <112431469+gabrielMFCR@users.noreply.github.com>
2023-03-26 12:47:56 -0300

    exercicio_html_js

---
+++ main.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form-numerico');
+
+function validaA(numeroA) {
+    const numeroComoArray = numeroA.split(' ')
+    return numeroComoArray.length > B;
+}
+
+
+
+
+console.log(form);
