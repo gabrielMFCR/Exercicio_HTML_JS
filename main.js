d3abd6125b992fa3c01de4056ee6da0be4a56270
gabrielMFCR <112431469+gabrielMFCR@users.noreply.github.com>
2023-04-17 13:10:31 -0300

    exercicio_html_js

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-const form = document.getElementById('form-numerico');
+const formulario = document.querySelector('form');
 
-function validaA(numeroA) {
-    const numeroComoArray = numeroA.split(' ')
-    return numeroComoArray.length > B;
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault();
+});
+
+
+function validarFormulario() {
+    var campoA = document.getElementById("num-a").value;
+    var campoB = document.getElementById("num-b").value;
+    var mensagem = document.getElementById("mensagem");
+
+    if (campoB > campoA) {
+        mensagem.innerHTML = "Formulário válido";
+        mensagem.className = "valido";
+        return true;
+    } else {
+        mensagem.innerHTML = "Formulário inválido! O valor do número B deve ser maior que o número A.";
+        mensagem.className = "invalido";
+        return false;
+
+    }
 }
-
-
-
-
-console.log(form);
